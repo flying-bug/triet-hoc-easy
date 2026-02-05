620c150f775563748e45f1c92d122cb362f414cf
duybo <boddhe187150@fpt.edu.vn>
2026-02-05 23:35:27 +0700

    update code lich van su

--- header.js
+++ header.js
@@ -7,9 +7,10 @@ function createHeader(activePage = '') {
                     <h1 class="nav-title">Triết Học</h1>
                     <ul class="nav-menu">
                         <li><a href="index.html" ${activePage === 'home' ? 'class="active"' : ''}>Trang Chủ</a></li>
+                        <li><a href="calendar.html" ${activePage === 'calendar' ? 'class="active"' : ''}>Lịch</a></li>
                         <li><a href="index.html#topics" ${activePage === 'topics' ? 'class="active"' : ''}>Chủ Đề</a></li>
                         <li><a href="quiz.html" ${activePage === 'quiz' ? 'class="active"' : ''}>Ôn Thi</a></li>
-                        <li><a href="calendar.html" ${activePage === 'calendar' ? 'class="active"' : ''}>Lịch</a></li>
+                        
                         <li><a href="index.html#about" ${activePage === 'about' ? 'class="active"' : ''}>Giới Thiệu</a></li>
                     </ul>
                 </div>
